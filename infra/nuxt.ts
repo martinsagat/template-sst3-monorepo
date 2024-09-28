3a79f00b957b1df162838edcc837e63274cc18ab
Martin Sagat <martin.sagat@outlook.com.au>
2024-09-28 15:05:17 +0800

    update config

--- infra/nuxt.ts
+++ infra/nuxt.ts
@@ -1,8 +1,15 @@
-export const nuxt = new sst.aws.Nuxt("NuxtApp", {
-    // domain: "domain.com",
+const domainName = "domain.com";
+
+export const nuxt = new sst.aws.Nuxt("Nuxt", {
+    domain: {
+        name: domainName,
+        dns: sst.aws.dns({
+            zone: "XXXXXXXXXXXX"
+        })
+    },
     path: "packages/nuxt",
     link: [],
     environment: {
-        API_URL: "https://api.homestep.com.au"
+        API_URL: "https://api." + domainName
     }
 });
